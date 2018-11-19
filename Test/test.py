2d1e64bc56e6e9b5b74e6d4b6709dce155358392
AlexanderSchober <alex.schober@mac.com>
2018-11-19 13:56:44 +0100

    edited travis

--- Test/test.py
+++ Test/test.py
@@ -1,1 +1,9 @@
-print('test')
+import unittest
+import mieze_python
+from mieze_python.mieze import Mieze
+
+class TestLaunch(unittest.TestCase):
+
+    def test_launch_version(self):
+        self.assertEqual(mieze_python.__version__, '0.0.1')
+        
