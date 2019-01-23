27575c77366fed1b383db3663707bf9d33cd7110
AlexanderSchober <alex.schober@mac.com>
2019-01-23 17:55:45 +0100

    Fixed test to account for version change

--- Tests/test.py
+++ Tests/test.py
@@ -5,4 +5,4 @@ from miezepy.mieze import Mieze
 class Test_launch(unittest.TestCase):
 
     def test_launch_version(self):
-        self.assertEqual(miezepy.__version__,'0.0.1')
+        self.assertEqual(miezepy.__version__,'0.1.0')
